dfffb79de7ea9f48e0e58da2c386cc29f520a2c6
猫猫 <2775921733@qq.com>
2025-08-31 00:11:17 +0800

    fix

--- distillation/gen_prototype_tinyimgnt.sh
+++ distillation/gen_prototype_tinyimgnt.sh
@@ -1,9 +1,9 @@
 python gen_prototype.py \
     --batch_size 10 \
     --data_dir /data2/wlf/datasets/tiny-imagenet \
-    --dataset tiny-imagenet \
-    --diffusion_checkpoints_path jcplus/stable-diffusion-v1-5 \
+    --dataset tiny_imagenet \
+    --diffusion_checkpoints_path stable-diffusion-v1-5/stable-diffusion-v1-5 \
     --ipc 10 \
     --km_expand 1 \
-    --label_file_path ./label-prompt/imagenet_classes.txt \
+    --label_file_path ./label-prompt/tinyimgnt-label.txt \
     --save_prototype_path ./prototypes
